9b45c3eb8b299eca8167e770cd32f836643345f9
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-10 05:43:12 +0700

    Fix

--- sample_django_app/config/settings/test.py
+++ sample_django_app/config/settings/test.py
@@ -1,1 +1,11 @@
-from .base import * 
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432
+    }
+}
